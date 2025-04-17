942cc8aee70b93d8efb90808bd8283c25b58091a
UI Bakery <test@test.com>
2025-04-17 14:24:40 +0200

    test

---
+++ pages/home/actions/newAction/code.js
@@ -0,0 +1,19 @@
+const list_data = [
+  {
+    id: 1,
+    status: 'paid',
+    fullName: 'Danielle Kennedy',
+    userName: 'danielle.kennedy',
+    email: 'danielle_91@example.com',
+    createdAt: '2021-01-13T00:00:23Z'
+  },
+  {
+    id: 2,
+    status: 'blocked',
+    fullName: 'Russell Payne',
+    userName: 'russell.payne',
+    email: 'russell_88@example.com',
+    createdAt: '2021-01-20T11:10:56Z'
+  }
+];
+ui.listView.setValue([{name: '4'}])
